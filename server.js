d5548383004c523cc336f8fa8099d6fd47efa96b
ctsai-kf <57999576+ctsai-kf@users.noreply.github.com>
2025-04-21 17:59:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require('http');
 var fs = require('fs');
 var express = require('express');
 const { MongoClient } = require('mongodb');
 var path = require('path');
 const app = express();
+
 const port = process.env.PORT || 8080;
+const apiKey = '&apikey=EG6KDTVRKHJECZUV';
 
 async function fetchStock(res, selectType, symbol) {
     const uri = "mongodb+srv://jaxtsai:Mongo123@cluster0.3sm3y1e.mongodb.net/?appName=Cluster0";        
@@ -72,6 +73,112 @@ async function fetchStock(res, selectType, symbol) {
 
     }
 
+    async function runGet(url) {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error("Fetching data failed:", error);
+        throw error;
+      }
+    }
+
+
+
+    async function getQuote(symbol) {
+      var url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + symbol +apiKey;
+      console.log("getQuote:" + symbol);
+
+      const data = await runGet(url);
+      console.log(data['Global Quote']['05. price']);
+      return data['Global Quote']['05. price'];        
+    }
+
+    async function getTicker(selectType, symbol) {
+
+        var url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=' + symbol + apiKey;
+        console.log("getTicker:" + symbol);
+        console.log(url);
+
+        const data = await runGet(url);
+        const array = data['bestMatches'];
+        const ret =[];
+        const key = (selectType === 'ticker') ? '1. symbol' : '2. name';
+        for (let i =0; i < array.length; i++) {
+          if (array[i][key] === symbol) {
+            ret.push({ "ticker": array[i]['1. symbol'], "company":  array[i]['2. name'] })
+          }
+        }
+        return ret;    
+    }
+
+    async function fetchStockWithAPI(res, selectType, symbol) {     
+      const tt = await getTicker(selectType, symbol);
+
+      console.log("tt:"+ tt);
+      let tableHTML = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>Dynamic Table</title>
+      </head>
+      <body>
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Ticker</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+    `;
+    if (tt.length ==0) {
+      const ticker= (selectType === 'ticker')?  symbol : null;
+      const company = (selectType === 'company')? symbol:  null;
+      console.log("Not found");
+      tableHTML += `
+      <tr>
+        <td>${company}</td>
+        <td>${ticker}</td>
+        <td>Not Found</td>
+      </tr>`;
+
+    } else {
+      // Print a message if no documents were found
+      for (let i = 0; i < tt.length; i++) {
+        const ticker= tt[i]['ticker'];
+        const company = tt[i]['company'];
+        let quote = await getQuote(tt[i]['ticker']);
+        console.log("quote:" + quote);
+
+        tableHTML += `
+        <tr>
+          <td>${company}</td>
+          <td>${ticker}</td>
+          <td>${quote}</td>
+        </tr>`;
+      }
+    }
+      // Print returned documents
+  
+
+      tableHTML += `
+      </tbody>
+      </table>
+      </body>
+      </html>
+      `;
+  
+      res.end(tableHTML);
+  
+      }
+
+
 app.get('/process', (req, res) => {
   const selectType = req.query.selectType;
   const symbol = req.query.symbol;
@@ -79,6 +186,14 @@ app.get('/process', (req, res) => {
   console.log('symbol:', symbol);  
   fetchStock(res, selectType, symbol);
 
+});
+
+app.get('/processAPI', (req, res) => {
+  const selectType = req.query.selectType;
+  const symbol = req.query.symbol;
+  console.log('selectType:', selectType);
+  console.log('symbol:', symbol);  
+  fetchStockWithAPI(res, selectType, symbol);
 });
 app.get('/', (req, res) => {
   const formData = req.body;
